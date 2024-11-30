5a20fee63bc772074dfbae32ddffcd065006cf18
yetimeh <senukabandara2006@gmail.com>
2024-11-30 12:31:36 +0530

    Fix tailwind vscode autocomplete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-tailwind.config = {
+// THIS FILE IS FOR VSCODE AUTO COMPLETION.
+// Since I'm not using node I have to duplicate all configuration here aswell in order for
+// autocompletion to work.
+
+
+module.exports = {
     theme: {
       extend: {
         colors: {
-          
         }
       }
     }
